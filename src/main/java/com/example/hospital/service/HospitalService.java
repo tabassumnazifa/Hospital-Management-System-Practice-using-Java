f59e5f9081daeb253d670c873400cbc72bd7f60d
Nazifa Tabassum <132453499+tabassumnazifa@users.noreply.github.com>
2026-08-12 17:52:40 +0600

    new

--- src/main/java/com/example/hospital/service/HospitalService.java
+++ src/main/java/com/example/hospital/service/HospitalService.java
@@ -71,7 +71,7 @@ public class HospitalService {
         return patientRepo.findAll(pageable)
                 .map(patient -> new PatientResponseDto(
                         patient.getId(),
-                        patient.getName(),
+                        patient.getName(),       
                         patient.getAge()
                 ));
     }
